b75cb8b11eb723481e5bfab4d6ca32d9b458453c
Louise Brunner <brunner.louise@gmail.com>
2019-09-02 17:28:02 +0200

    Adds new parameter to wordApi

--- functions/test/http/wordApi.function.js
+++ functions/test/http/wordApi.function.js
@@ -10,9 +10,11 @@ exports = module.exports = functions
   .https.onRequest(async (req, res) => {
     const ID_API = '4e2d6b7ecfmsh040d36b67dfc940p12acabjsnf88bb7542868'
     const query = req.query.query
-    console.log(query)
+    const action = req.query.action
+    console.log([query, action])
+
     const promise_word = await axios.get(
-      `https://wordsapiv1.p.rapidapi.com/words/${query}/rhymes`,
+      `https://wordsapiv1.p.rapidapi.com/words/${query}/${action}`,
       {
         method: 'GET',
         headers: {
@@ -21,18 +23,30 @@ exports = module.exports = functions
         }
       }
     )
-    const response_word = promise_word.data.rhymes.all
-
-    const result_small_word = response_word.filter((word) => {
+    
+    const response_word = promise_word.data
+    console.log("hello") 
+    if (action === 'synonyms'){ 
+      response_action = response_word.synonyms
+    } else if (action === 'rhymes') {
+      response_action = response_word.rhymes.all
+    } else if (action === 'antonyms'){
+      response_action = response_word.antonyms
+    }
+    //  else if (action === 'definitions'){
+    //   response_action_definitions = response_word.definitions
+    // } 
+  
+    const result_small_word = response_action.filter((word) => {
       return word.length <= 4
     })
-    const result_medium_word = response_word.filter((word) => {
+    const result_medium_word = response_action.filter((word) => {
       return word.length > 3 && word.length <= 5
     })
-    const result_large_word = response_word.filter((word) => {
+    const result_large_word = response_action.filter((word) => {
       return word.length > 5 && word.length <= 7
     })
-    const result_xl_word = response_word.filter((word) => {
+    const result_xl_word = response_action.filter((word) => {
       return word.length > 7
     })
     res.send([
@@ -42,3 +56,4 @@ exports = module.exports = functions
       ...result_xl_word
     ])
   })
+
